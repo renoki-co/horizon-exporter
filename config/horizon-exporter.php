294fbed6fc19eb3cd28f9a59c5c5d3542a4371f0
Alex Renoki <rennokki@gmail.com>
2021-09-25 21:36:48 +0300

    config [skip ci]

--- config/horizon-exporter.php
+++ config/horizon-exporter.php
@@ -48,6 +48,9 @@ return [
     | If you periodically run the horizon:snapshot command, it will reset
     | the values exposed to Prometheus to 0, this leading to Prometheus
     | reading counters starting from 0 each time it is ran.
+    | Because most of the counters are gauges, this means that once they are
+    | reset, running rate() methods on given gauges will show dropped
+    | values to 0, making a confusion to the actual rate of metrics.
     |
     | You can prevent that by enabling this setting. On the current values
     | for throughput, jobs, etc., the values from snapshots will also be
